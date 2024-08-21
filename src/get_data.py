ba9925681643ed24f0a66c3cc9984edcbeb14b0b
yakovlevs01 <yakovlevs01@yandex.ru>
2024-08-21 11:39:26 +0300

    rewritten with pathlib

--- src/get_data.py
+++ src/get_data.py
@@ -1,13 +1,16 @@
-import os
+from pathlib import Path
 
 from exceptions import InvalidDataError, LogFileNotFoundError
 
 
-def read_log_file(file_path: str, num_lines: int = 4) -> list[str]:
+def read_log_file(file_path: Path, num_lines: int = 4) -> list[str]:
     log_lines = []
-    if not os.path.exists(file_path):
+    log_file = Path(file_path)
+
+    if not log_file.exists():
         raise LogFileNotFoundError(f"Failed to locate log path. Check {file_path}.")
-    with open(file_path) as log_file:
+
+    with log_file.open() as log_file:
         for _ in range(num_lines):
             line = log_file.readline()
             if not line:
@@ -20,14 +23,14 @@ def locate_game_path(set_path: str = "") -> str:
     if set_path:
         return set_path
 
-    app_data = os.getenv("APPDATA")
-    locallow_path = os.path.abspath(os.path.join(app_data, "..", "LocalLow", "miHoYo", "ZenlessZoneZero"))
+    app_data_path = Path.home() / "AppData"
+    locallow_path = (app_data_path / "LocalLow" / "miHoYo" / "ZenlessZoneZero").resolve()
 
-    log_path = os.path.join(locallow_path, "Player.log")
+    log_path = locallow_path / "Player.log"
     log_lines = read_log_file(log_path)
 
     if not log_lines:
-        log_path = os.path.join(locallow_path, "Player-prev.log")
+        log_path = locallow_path / "Player-prev.log"
         log_lines = read_log_file(log_path)
 
     if not log_lines:
