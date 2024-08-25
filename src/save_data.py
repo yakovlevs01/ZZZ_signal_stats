29ca94280720ac48023e92a8cfa0a30896c3568a
yakovlevs01 <yakovlevs01@yandex.ru>
2024-08-25 12:17:11 +0300

    tests

--- src/save_data.py
+++ src/save_data.py
@@ -1,6 +1,6 @@
 import pandas as pd
 
-from get_data import get_everything
+from get_data import get_whole_gacha_data_by_type, init
 
 
 def clear_data(df: pd.DataFrame) -> pd.DataFrame:
@@ -8,7 +8,12 @@ def clear_data(df: pd.DataFrame) -> pd.DataFrame:
 
 
 if __name__ == "__main__":
-    raw_signal_data = get_everything()
-    signal_data_df = pd.DataFrame(raw_signal_data)
-    signal_data_df = clear_data(signal_data_df)
-    signal_data_df.to_excel("ZZZ_import.xlsx")
+    game_path, url = init()
+    gacha_types = {"standart": 1, "event": 2, "weapon": 3, "idk": 4, "banbu": 5}
+    for g_type in gacha_types:
+        raw_signal_data = get_whole_gacha_data_by_type(url, g_type)
+        if raw_signal_data:
+            signal_data_df = pd.DataFrame(raw_signal_data)
+            signal_data_df = clear_data(signal_data_df)
+            print(signal_data_df)
+            signal_data_df.to_excel(f"ZZZ_import_{g_type}.xlsx")
