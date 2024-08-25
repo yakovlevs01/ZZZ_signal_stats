5397048f41467b3651031ab411ad25699e96fdb3
yakovlevs01 <yakovlevs01@yandex.ru>
2024-08-25 10:52:35 +0300

    test for pre-commit

--- src/back.py
+++ src/back.py
@@ -5,7 +5,8 @@ app = FastAPI()
 
 
 @app.get("/data")
-async def get_data():
-    df = pd.read_excel("zzz_logs1.xlsx")
-    print(df)
-    return df.to_dict(orient="records")
+async def get_data() -> list[dict]:
+    logs = pd.read_excel("zzz_logs1.xlsx")
+    print(logs)
+
+    return logs.to_dict(orient="records")
